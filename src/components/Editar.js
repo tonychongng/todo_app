15e9e9ad34bf6f7a7483b190cb8cce5bb5ecab9e
tony123x <tonychongng123@gmail.com>
2023-09-10 17:33:09 +0200

    update

---
+++ src/components/Editar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Editar = () => {
+  return (
+    <form>
+        <input type='text'/>
+        <input type='submiot' value="submit"/>
+    </form>
+  )
+}
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Editar } from './Editar';
 
 
 export const Listado = ({todoList,setTodoList}) => {
+
+  const [editar, setEditar] = useState(0);
+
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [editedText, setEditedText] = useState('');
 
   const deleteTodo = (e, todo) => {
     const lista = todoList;
@@ -11,21 +17,83 @@ export const Listado = ({todoList,setTodoList}) => {
     setTodoList(nuevaLista);
   }
 
+  const editTodo = (e, todoId) => {
+    setEditingTodoId(todoId);
+
+    const todo = todoList.find(item => item.id === todoId);
+
+    setEditedText(todo.title);
+
+  }
+
+  const saveEditedTodo = (e, todoId) => {
+    const todoIndex = todoList.findIndex(item => item.id === todoId);
+
+    if(todoIndex !== 1) {
+      const nuevaLista = [...todoList];
+
+      nuevaLista[todoIndex].title = editedText;
+
+      setTodoList(nuevaLista);
+
+      setEditingTodoId(null);
+      setEditedText('');
+    }
+  }
+
 
   return (
     <>
       {
         todoList.map(todo => {
           return (
-            <div key={todo.id} className='todo' >
-              <p>{todo.title}</p>  
-              <input 
-                type='button' 
-                value="X" 
+            <div key={todo.id} className='todo'>
+              {editingTodoId === todo.id ? (
+                // Si estamos editando, muestra el input
+                <input
+                  className='on-editing'
+                  type="text"
+                  value={todo.title}
+                  onChange={(e) => {
+                    const nuevaLista = todoList.map(item => {
+                      if (item.id === todo.id) {
+                        return { ...item, title: e.target.value };
+                      }
+                      return item;
+                    });
+                    setTodoList(nuevaLista);
+                  }}
+                />
+
+              ) : (
+                // Si no estamos editando, muestra el texto
+                <p>{todo.title}</p>
+              )}
+
+
+              <input
+                type='button'
+                value="X"
                 className='delete'
-                onClick={e => {deleteTodo(e, todo.title)}}
+                onClick={(e) => deleteTodo(e, todo.title)}
               />
-              <input type='button' value="Edit" className='edit'/>
+              {editingTodoId === todo.id ? (
+                // Si estamos editando, muestra el botón de guardar
+                <input
+                  type='button'
+                  value="Guardar"
+                  className='edit'
+                  onClick={() => setEditingTodoId(null)}
+                />
+              ) : (
+                // Si no estamos editando, muestra el botón de editar
+                <input
+                  type='button'
+                  value="Editar"
+                  className='edit'
+                  onClick={() => editTodo(null, todo.id)}
+                />
+              )}
             </div>
           )
         })
